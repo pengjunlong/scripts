27f9655ab976ddc1b1b35bdb1300275083b92c17
pengjunlong <pengjunlong@meituan.com>
2024-12-11 18:31:13 +0800

    deduplicate

--- scrape.py
+++ scrape.py
@@ -16,41 +16,39 @@ def fetch_text(url):
     return text
 
 def save_text(text, filename):
-    with open(filename, 'w') as file:
+    with open(filename, 'w', encoding='utf-8') as file:
         file.write(text)
 
 def generate_v2rayshare_url():
     today = datetime.today()
     return f"https://v2rayshare.githubrowcontent.com/{today.year}/{today.month:02d}/{today.strftime('%Y%m%d')}.txt"
 
-
 def v2rayshare():
     url = generate_v2rayshare_url()
     text = fetch_text(url)
     return text
-
 
 def aiboboxx():
     url = "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2"
     text = fetch_text(url)
     return text
 
-
 def ermaozi():
     url = "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt"
     text = fetch_text(url)
     return text
 
+def deduplicate(text):
+    lines = text.split('\n')
+    lines = [line.split('#')[0].strip() for line in lines if line.strip()]
+    lines = sorted(set(lines))
+    return '\n'.join(lines)
 
 if __name__ == "__main__":
     text = ""
     text += v2rayshare()
     text += aiboboxx()
     text += ermaozi()
-    text = text.rstrip('\n')
+    text = deduplicate(text)
     text = base64.b64encode(text.encode('utf-8')).decode('utf-8')
     save_text(text, "v2ray.txt")
-
-
-
-
