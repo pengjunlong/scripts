812fc57acaf2ecb8f039c46fb0d8a07a89f5f779
pengjunlong <pengjunlong@meituan.com>
2024-12-11 17:44:25 +0800

    base64

--- scrape.py
+++ scrape.py
@@ -1,21 +1,56 @@
 import requests
+import base64
 from bs4 import BeautifulSoup
 from datetime import datetime
 
 def fetch_text(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
-    return soup.get_text()
+    text = soup.get_text()
+    try:
+        text = base64.b64decode(text).decode('utf-8')
+    except Exception:
+        print("base64 decode failed")
+    if not text.endswith('\n'):
+        text += '\n'
+    return text
 
 def save_text(text, filename):
     with open(filename, 'w') as file:
         file.write(text)
 
-def generate_url():
+def generate_v2rayshare_url():
     today = datetime.today()
     return f"https://v2rayshare.githubrowcontent.com/{today.year}/{today.month:02d}/{today.strftime('%Y%m%d')}.txt"
 
-if __name__ == "__main__":
-    url = generate_url()
+
+def v2rayshare():
+    url = generate_v2rayshare_url()
     text = fetch_text(url)
-    save_text(text, "v2rayshare.txt")
+    return text
+
+
+def aiboboxx():
+    url = "https://raw.githubusercontent.com/aiboboxx/v2rayfree/main/v2"
+    text = fetch_text(url)
+    return text
+
+
+def ermaozi():
+    url = "https://raw.githubusercontent.com/ermaozi/get_subscribe/main/subscribe/v2ray.txt"
+    text = fetch_text(url)
+    return text
+
+
+if __name__ == "__main__":
+    text = ""
+    text += v2rayshare()
+    text += aiboboxx()
+    text += ermaozi()
+    text = text.rstrip('\n')
+    text = base64.b64encode(text.encode('utf-8')).decode('utf-8')
+    save_text(text, "v2ray.txt")
+
+
+
+
